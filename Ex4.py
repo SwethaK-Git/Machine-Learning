3adaab27c54155f644f152e54644964f17452113
SwethaK-Git <swethak1905@gmail.com>
2025-08-18 20:22:50 +0530

    Add files via upload

---
+++ Ex4.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+
+
+data=pd.read_csv('attendance.csv')
+
+df=pd.DataFrame(data)
+
+
+x=df[['study_hrs','attendance']]
+
+y=df['result']
+
+dtc=DecisionTreeClassifier(criterion='entropy',random_state=0)
+
+dtc.fit(x,y)
+
+plt.figure(figsize=(8,5))
+
+plot_tree(dtc,feature_names=['study_hrs','attendance'],class_names=['Fail','Pass'],filled=True)
+
+plt.show()
+
+
+new=[[5,85]]
+
+pred=dtc.predict(new)
+
+print("Prediction for new student:","1" if pred[0]==1 else "0")
